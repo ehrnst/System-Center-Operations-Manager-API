6f5f44e05f9b037b1cdc3496c8958be3bde3c00b
Martin Ehrnst <martin@ehrnst.no>
2017-05-05 14:39:30 +0200

    Response code

--- Controllers/SCOMMaintenanceController.cs
+++ Controllers/SCOMMaintenanceController.cs
@@ -36,8 +36,11 @@ namespace SCOM_API.Controllers
         ///     "Minutes": 10,
         ///     "comment": "doing maintenance"
         /// }
-    /// </example>
-    [HttpPost]
+        /// </example>
+        /// <response code="201">Successfully added maintenance mode for computer</response>
+        /// <response code="400">Bad request. Check json input</response>
+        /// <response code="409">Conflict computer already in maintenance</response>
+        [HttpPost]
         [Route("API/ComputerMaintenance")]
         public IHttpActionResult EnableComputerMaintenance(SCOMComputerMaintenanceModel Data)
         {
@@ -59,7 +62,7 @@ namespace SCOM_API.Controllers
                 if (!monObject.InMaintenanceMode)
                 {
                     {
-                        ///set maintenance properties
+                        //set maintenance properties
                         DateTime startTime = DateTime.UtcNow;
                         DateTime schedEndTime = DateTime.UtcNow.AddMinutes(Data.Minutes);
                         MaintenanceModeReason reason = MaintenanceModeReason.PlannedOther;
@@ -67,22 +70,20 @@ namespace SCOM_API.Controllers
 
                         monObject.ScheduleMaintenanceMode(startTime, schedEndTime, reason, comment);
 
-                        ///Add properties to list
+                        //Add properties to list
                         SCOMComputerMaintenanceModel maintenanceComputer = new SCOMComputerMaintenanceModel();
                         maintenanceComputer.DisplayName = monObject.DisplayName;
                         maintenanceComputer.EndTime = schedEndTime;
                         maintenanceComputer.Minutes = Data.Minutes;
                         maintenanceComputer.comment = comment;
                         
-                        ///add computers to list
+                        //add computers to list
                         MaintenanceComputers.Add(maintenanceComputer);
 
                     }
-                    ///Return list of computers as Json
-                    return Json(MaintenanceComputers);
                 }
 
-            ///If computer already in maintenance. Do nothing and list info
+            //If computer already in maintenance. Do nothing and list info
             else
                 {
                     MaintenanceWindow MaintenanceWindow = monObject.GetMaintenanceWindow();
@@ -95,11 +96,15 @@ namespace SCOM_API.Controllers
 
                     MaintenanceComputers.Add(maintenanceComputer);
 
+                    throw new HttpResponseException(Request
+                .CreateResponse(HttpStatusCode.Conflict));
+
                 }
-            ///Return list of computers as Json
+
+            //Return list of computers as Json
             return Json(MaintenanceComputers);
 
         }
 
     }
-}///END
+}//END
