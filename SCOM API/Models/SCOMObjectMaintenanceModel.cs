9f6f2f1b0a3ed74c209aa877b551bfa155aef443
Martin Ehrnst <martin@ehrnst.no>
2017-05-22 13:53:05 +0200

    Added maintenance scheduling

--- SCOM API/Models/SCOMObjectMaintenanceModel.cs
+++ SCOM API/Models/SCOMObjectMaintenanceModel.cs
@@ -7,14 +7,21 @@ namespace SCOM_API.Models
 {
     public class SCOMObjectMaintenanceModel
     {
+        /// <summary>
+        /// Monitoring object id
+        /// </summary>
         public string id { get; set; }
 
         public string displayName { get; set; }
-
+        /// <summary>
+        /// Minutes to maintenance
+        /// </summary>
         public int Minutes { get; set; }
 
         public DateTime EndTime { get; set; }
-
+        /// <summary>
+        /// Maintenance comment
+        /// </summary>
         public string comment { get; set; }
     }
 }
